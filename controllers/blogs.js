4d472e1e9ea6e88714b854ed497118f5491429d8
Atstincer <atstincer87@gmail.com>
2026-07-23 11:51:05 +0200

    6. Moar likes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -18,14 +18,26 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.delete('/:id', async (req, res) => {
-  console.log('blogs id to  delete', req.params.id)
-  const blog = await Blog.findByPk(req.params.id)
-  if (!blog) {
-    return res.status(404).json({ error: 'Resource not found' })
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  if (!req.blog) {
+    return res.status(404).end()
   }
-  await blog.destroy()
+  next()
+}
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  await req.blog.destroy()
   return res.status(200).json({ message: 'Resource deleted' })
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (!req.body.likes) {
+    return res.status(400).end()
+  }
+  req.blog.likes = req.body.likes
+  await req.blog.save()
+  return res.json(req.blog)
 })
 
 module.exports = router
