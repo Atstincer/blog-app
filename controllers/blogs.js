18cdfb4bb8bdc5ed97f31709be11b80eb13bef67
Atstincer <atstincer87@gmail.com>
2026-07-25 17:27:48 +0200

    10. Blog ownership

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,17 +1,42 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
 
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
 
+//gets all blogs
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ['userId'] },
+    include: {
+      model: User,
+      attributes: ['name'],
+    },
+  })
   //console.log(blogs)
   res.json(blogs)
 })
 
-router.post('/', async (req, res, next) => {
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
+//creates a new blog asociated with the logged in user
+router.post('/', tokenExtractor, async (req, res, next) => {
   //console.log('req.body', req.body)
   try {
-    const blog = await Blog.create({ ...req.body })
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = await Blog.create({ ...req.body, userId: user.id })
     return res.json(blog)
   } catch (error) {
     //return res.status(400).json({ error })
@@ -19,6 +44,7 @@ router.post('/', async (req, res, next) => {
   }
 })
 
+//blog finder middlerware
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
   if (!req.blog) {
@@ -27,11 +53,13 @@ const blogFinder = async (req, res, next) => {
   next()
 }
 
+//deletes a blog
 router.delete('/:id', blogFinder, async (req, res) => {
   await req.blog.destroy()
   return res.status(200).json({ message: 'Resource deleted' })
 })
 
+//changes the likes of a blog
 router.put('/:id', blogFinder, async (req, res, next) => {
   console.log('req.blog', req.blog.toJSON())
   try {
