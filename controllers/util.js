eee3a7e129ac585c187102e47bbd511426ba2ac2
Atstincer <atstincer87@gmail.com>
2026-07-29 10:59:53 +0200

    adding cascade: true

--- controllers/util.js
+++ controllers/util.js
@@ -2,8 +2,8 @@ const router = require('express').Router()
 const { Blog, User } = require('../models')
 
 router.post('/api/reset', async (req, res) => {
-  await Blog.destroy({ truncate: true })
-  await User.destroy({ truncate: true })
+  await Blog.destroy({ truncate: true, cascade: true })
+  await User.destroy({ truncate: true, cascade: true })
   return res.status(204).end()
 })
 
