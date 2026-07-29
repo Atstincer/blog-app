b59aef55cdfd638908b574912487b57a7fe950a9
Atstincer <atstincer87@gmail.com>
2026-07-29 10:23:33 +0200

    changing model.sync for sequelize.sync

--- models/index.js
+++ models/index.js
@@ -1,11 +1,14 @@
 const Blog = require('./blog')
 const User = require('./user')
+const { sequelize } = require('../util/db')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-User.sync({ alter: true })
-Blog.sync({ alter: true })
+//User.sync({ alter: true })
+//Blog.sync({ alter: true })
+
+sequelize.sync({ alter: true })
 
 module.exports = {
   Blog,
